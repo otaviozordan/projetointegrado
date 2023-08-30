9385e88a9fdca2558b096f7c97801e2254df3ba0
Otávio Zordan Alves <otavio.zordan@outlook.com>
2023-08-30 09:01:25 -0300

    Vai filhao

--- projetoIntegrado.py
+++ projetoIntegrado.py
@@ -1,16 +1,18 @@
+import math
 import requests
-from retrying import retry
-from OFDM_64_QAM import *
-from colorama import Fore, Style
 import json
+from retrying import retry
+from colorama import Fore, Style
+
+from OFDM_64_QAM import *
 
 ip = '196.168.0.103'
-token = '/token=teleco2k23g1'
+token = '/token=teleco2k23g5'
 url1 = 'http://' + ip + ':8000' + token
 url2 = 'http://' + ip + ':8020' + token
 
 # Função para fazer uma requisição HTTP com tentativas
-@retry(wait_fixed=5000, stop_max_attempt_number=3)
+@retry(wait_fixed=2000, stop_max_attempt_number=3)
 def make_request(url):
     print(Fore.BLUE + "Tentando URL:", url + Style.RESET_ALL)
     response = requests.get(url)
@@ -55,17 +57,25 @@ try:
 
     if dicInfo['BER'] <= padrao:
         mensagem += 'apto'
+        usavel = True
     else: 
         mensagem += 'inviavel'
+        usavel = False
+
+    # Função para converter Watts em dBm
+    def watts_to_dbm(watts):
+        dbm = 10 * math.log10(watts * 1000)
+        return dbm
 
     print(Fore.GREEN + mensagem + ' para padrao ' + str(padrao) + Style.RESET_ALL)
     jsonResposta = {
         'server': ['CarrierSRS', 'ServiceSRS'],
         'ber': dicInfo['BER'],
-        'Potencia RX em dBm': dicInfo['S_Power'],
-        'Potencia do ruido em dBm': dicInfo['N_Power'],
+        'Potencia_RX': str(watts_to_dbm(float(dicInfo['S_Power'])))+ ' [dBm]',
+        'Potencia_do_ruido': str(watts_to_dbm(float(dicInfo['N_Power'])))+' [dbm]',
         'ber': dicInfo['BER'],
         'servico': servico,
+        'usavel':usavel,
         'mensagem': mensagem,
         'modulacao':modulation
     }
@@ -75,3 +85,9 @@ try:
 
 except Exception as e:
     print(Fore.RED + f'[ERRO] {str(e)}' + Style.RESET_ALL)
+    jsonResposta = {
+        'mensagem': f'[ERRO] {str(e)}',
+    }
+
+    # Imprimir o JSON identado
+    print(Fore.YELLOW + json.dumps(jsonResposta, indent=4) + Style.RESET_ALL)
